003bcd8062ab58f249c4e1636cc1bd2b4fa21a13
proteoxx <franciscoarocha@MacBook-Pro-de-Francisco.local>
2024-08-25 20:24:16 -0300

    Primer commit

---
+++ 00_hello.py
@@ -0,0 +1,15 @@
+# Hola Mundo 
+print("Hola Python")
+
+"""
+Comentario en varias 
+Líneas.
+"""
+
+print(type("Caracter")) # as str
+print(type(5)) # as int
+print(type(5.1)) # as float
+print(type(True)) # as bool
+# print(type(x+2x)) # as str
+
+
---
+++ 01_variables.py
@@ -0,0 +1,10 @@
+# Variables
+my_int_variable =5
+my_str_cratacte="Caracter"
+
+print(my_str_cratacte) 
+print(type(my_str_cratacte))
+
+name, apellido, nickname, mail = "Francisco", "Arocha", "PrOteOx", "fcoarochaa@gmail.com"
+
+print("Me llamo",name,apellido,"y me dicen",nickname,", mi correo es",mail)
